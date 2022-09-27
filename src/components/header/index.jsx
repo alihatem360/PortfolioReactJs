76f8407c4221e75dcebb089f830f7aad95619654
ali hatem <alihatem7201737@gmail.com>
2022-09-27 21:41:35 +0200

    make data dinamice

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,43 +1,43 @@
 import "./style.css";
-import logo from "../../images/header-background2.jpeg";
 import { useEffect, useState } from "react";
 import SocialData from "../../data/projects";
 const Header = () => {
   const [socialsData, setSocialsData] = useState([]);
+  const [aboutmeData, setAboutmeData] = useState([]);
   useEffect(() => {
     SocialData.getSocials().then((data) => {
       setSocialsData(data[0]);
     });
+
+    SocialData.getAboutme().then((data) => {
+      setAboutmeData(data[0][0]);
+    });
   }, []);
 
+  console.log(aboutmeData);
   const mySocials = ["github", "linkedin", "twitter", "facebook", "youtube"];
-
+  console.log("header" + aboutmeData.name);
   return (
     <>
       <div className="header">
         <div className="row">
           <div className="col-lg-6 col-md-6 col-sm-12">
             <div className="header-img ">
-              <img src={logo} alt="placeholder" />
+              <img src={aboutmeData.heaaderimag} alt="placeholder" />
             </div>
           </div>
 
           <div class=" col-lg-6 col-md-6 col-sm-12">
             <div className="header-text">
               <h4>Hi👋</h4>
-              <h2>I'm Ali Hatem</h2>
-              <p>
-                I'm a Computer Science Student, interested in some CS branches
-                like programming and web development. I aim to Be a software
-                engineer. I have a good knowledge of HTML & CSS, JavaScript, and
-                React.js.
-              </p>
+              <h2>I'm {aboutmeData.name}</h2>
+              <p>{aboutmeData.bio}</p>
 
               <div className="header-buttons">
                 <a href="#" className="btn">
                   Hir me
                 </a>
-                <a href="#" className="btn">
+                <a href={aboutmeData.cv} target="_blank" className="btn">
                   Download CV
                 </a>
               </div>
