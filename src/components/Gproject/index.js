7d565306d86f15486ba0de4c10c85ad28c660a77
Ali Hatem <alihatemramadan5@gmail.com>
2024-12-29 15:53:54 +0200

    Refactor Gproject component to use className instead of class, improve code readability, and clean up CSS by removing commented styles.

--- src/components/Gproject/index.js
+++ src/components/Gproject/index.js
@@ -10,86 +10,82 @@ function index({ gproject }) {
   };
   // console.log(gproject, "gproject in gproject card");
   return (
-    <React.Fragment>
-      <div class="gproject_card_container">
-        <div class="gproject_card">
-          <h3>
-            <i class="fa-solid fa-rocket"></i>{" "}
-            {gproject === undefined || gproject.title === ""
-              ? null
-              : gproject.title}
-          </h3>
-          <div class="gproject_card_body">
-            <div class="gproject_card_video" data-aos="fade-up">
-              <iframe
-                src={
-                  gproject === undefined || gproject.video === ""
-                    ? null
-                    : `https://www.youtube.com/embed/${getVideoId(
-                        gproject.video
-                      )}`
-                }
-                frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                allowfullscreen="true"
-                title="YouTube video player"
-                width="100%"
-              ></iframe>
-            </div>
-            <div class="gproject_card_content" data-aos="fade-up">
-              {
-                <p>
-                  {gproject === undefined || gproject.description === "" ? (
-                    <p>no description</p>
-                  ) : (
-                    gproject.description
-                  )}
-                </p>
+    <div className="gproject_card_container">
+      <div className="gproject_card">
+        <h3>
+          <i className="fa-solid fa-rocket"></i>{" "}
+          {gproject === undefined || gproject.title === ""
+            ? null
+            : gproject.title}
+        </h3>
+        <div className="gproject_card_body">
+          <div className="gproject_card_video" data-aos="fade-up">
+            <iframe
+              src={
+                gproject === undefined || gproject.video === ""
+                  ? null
+                  : `https://www.youtube.com/embed/${getVideoId(
+                      gproject.video
+                    )}`
               }
-              <div className="card-buttons w-100 justify-content-start gap-5">
-                {gproject === undefined ||
-                gproject.github === undefined ? null : (
-                  <a
-                    className={
-                      gproject.codeStatus === "PRIVATE" ? "btn disabled" : "btn"
-                    }
-                    href={gproject.github}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <span>GitHub</span> <i class="fa-brands fa-github"></i>
-                  </a>
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen="true"
+              title="YouTube video player"
+              width="100%"
+            ></iframe>
+          </div>
+          <div className="gproject_card_content" data-aos="fade-up">
+            {
+              <p>
+                {gproject === undefined || gproject.description === "" ? (
+                  <p>no description</p>
+                ) : (
+                  gproject.description
                 )}
+              </p>
+            }
+            <div className="card-buttons w-100 justify-content-start gap-5">
+              {gproject === undefined ||
+              gproject.github === undefined ? null : (
+                <a
+                  className={
+                    gproject.codeStatus === "PRIVATE" ? "btn disabled" : "btn"
+                  }
+                  href={gproject.github}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span>GitHub</span> <i className="fa-brands fa-github"></i>
+                </a>
+              )}
 
-                {gproject === undefined ||
-                gproject.demo === undefined ? null : (
-                  <a
-                    className="btn"
-                    href={gproject.demo}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <span>Demo</span> <i class="fa-solid fa-globe"></i>
-                  </a>
-                )}
+              {gproject === undefined || gproject.demo === undefined ? null : (
+                <a
+                  className="btn"
+                  href={gproject.demo}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span>Demo</span> <i className="fa-solid fa-globe"></i>
+                </a>
+              )}
 
-                {gproject === undefined ||
-                gproject.video === undefined ? null : (
-                  <a
-                    className="btn"
-                    href={gproject.video}
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    <span>video</span> <i class="fa-solid fa-play"></i>
-                  </a>
-                )}
-              </div>
+              {gproject === undefined || gproject.video === undefined ? null : (
+                <a
+                  className="btn"
+                  href={gproject.video}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <span>video</span> <i className="fa-solid fa-play"></i>
+                </a>
+              )}
             </div>
           </div>
         </div>
       </div>
-    </React.Fragment>
+    </div>
   );
 }
 
